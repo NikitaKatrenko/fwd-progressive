04aa46b07599ee097cb7f71006d22c17a986dfb2
twoninth <thanqdj@gmail.com>
2024-07-07 16:55:09 +0300

    Firebase + Vite

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,11 @@ if ('serviceWorker' in navigator) {
         .then((reg) => console.log('service worker registered', reg))
         .catch((reg) => console.log('service worker not registered', reg))
 }
+
+navigator.serviceWorker.getRegistrations().then(
+    regs => {
+        for(const reg of regs) {
+            reg.unregister();
+        }
+    }
+)
